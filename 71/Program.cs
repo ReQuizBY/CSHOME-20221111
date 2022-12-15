10b9ad61d77d533ab83fad9cee86b375ebc8dac0
Столпец <requizby@gmail.com>
2022-12-16 01:37:52 +0300

    Lesson7home

---
+++ 71/Program.cs
@@ -0,0 +1,19 @@
+﻿// 71. Показать натуральные числа от N до 1, N задано
+
+string NumbersFor(int a, int b)
+{
+ string result = String.Empty;
+ for (int i = a; i >= b; i--)
+ {
+ result += $"{i} ";
+ }
+ return result;
+}
+string NumbersRec(int a, int b)
+{
+ if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+ else return String.Empty;
+}
+Console.WriteLine(NumbersFor(1, 10)); 
+Console.WriteLine(NumbersRec(1, 10)); 
+
---
+++ 75/Program.cs
@@ -0,0 +1,16 @@
+﻿// 75. Написать программу вычисления функции Аккермана
+
+int Func(int m, int n)
+		{
+			if(m == 0)
+			{
+				return n + 1;
+			}
+			if(m > 0 && n == 0)
+			{
+				return Func(m - 1, 1);
+			}
+			return Func(m - 1, Func(m, n - 1));
+		}
+		
+		System.Console.WriteLine(Func(2,2));
